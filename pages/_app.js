b3e8b986340333910c214a96377299f224f6f151
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-10-19 12:36:50 +0100

    coping app.js and index.js files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import Navbar from "../components/UI/Navbar";
+import { AuthContextProvider } from "../store/auth-context";
+import Head from "next/head";
+import theme from "../styles/theme";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Foodie | What are you cooking?</title>
+      </Head>
+      <AuthContextProvider>
+        <ChakraProvider>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Navbar />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </AuthContextProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
